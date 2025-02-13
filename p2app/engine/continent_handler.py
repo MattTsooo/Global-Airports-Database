509b181487943ee1bcd91735c74d874bd93c8cf1
matthewtso <tsomy@uci.edu>
2025-02-12 22:00:32 -0800

    Implemented continent_handler.py
    implemented continent_handler.py with the same logic from main

--- p2app/engine/continent_handler.py
+++ p2app/engine/continent_handler.py
@@ -1,12 +1,74 @@
-from p2app import *
+from p2app.events import *
 import sqlite3
+import database_handler
 
 class ContinentHandler:
-    def __init__(self, connection, event):
+    def __init__(self, connection):
         self._connection = connection
-        self._event = event
-        self._cursor.execute('PRAGMA integrity_check')
-        self._cursor.execute('PRAGMA foreign_keys = ON;')
 
 
-    def
+
+    def start_continent_search(self, event):
+        query_conditions = []
+        injection_params = {}
+
+        if event.continent_code():
+            query_conditions.append('continent_code = :con_code')
+            injection_params['con_code'] = event.continent_code()
+
+        if event.name():
+            query_conditions.append('name = :con_name')
+            injection_params['con_name'] = event.name()
+
+        query = 'SELECT * FROM continent'
+        if query_conditions:
+            query += ' WHERE ' + ' AND '.join(query_conditions)
+
+        cursor = self._connection.execute_queries(query, injection_params)
+
+        while True:
+            result = cursor.fetchone()
+            if result is None:
+                break
+            yield ContinentSearchResultEvent(Continent(result[0], result[1], result[2]))
+
+
+    def load_continent_event(self, event):
+        try:
+            cursor = self._connection.execute_queries('''SELECT *
+                                            FROM continent
+                                            WHERE continent_id = :id;''',
+                                {'id': event.continent_id()})
+            result = cursor.fetchone()
+            if result:
+                yield ContinentLoadedEvent(Continent(result[0], result[1], result[2]))
+
+        except sqlite3.Error:
+            yield ErrorEvent('FAILED TO LOAD CONTINENT')
+
+
+    def save_new_continent_event(self, event):
+        try:
+            con_id, con_code, con_name = event.continent()
+            self._connection.execute_queries('''INSERT INTO continent (continent_id, continent_code, name)
+                                        VALUES (:c_id, :c_code, :name);''',
+                                 {'c_id': con_id, 'c_code': con_code, 'name': con_name})
+            self._connection.commit()
+            yield ContinentSavedEvent(event.continent())
+
+        except sqlite3.Error:
+            yield SaveContinentFailedEvent('FAILED TO SAVE NEW CONTINENT')
+
+
+    def save_continent_event(self, event):
+        try:
+            con_id, con_code, con_name = event.continent()
+            self._connection.execute_queries('''UPDATE continent
+                                    SET continent_code = :c_code, name = :name
+                                    WHERE continent_id = :c_id;''',
+                                 {'c_id': con_id, 'c_code': con_code, 'name': con_name})
+            self._connection.commit()
+            yield ContinentSavedEvent(event.continent())
+
+        except sqlite3.Error:
+            yield SaveContinentFailedEvent('FAILED TO SAVE CONTINENT')
